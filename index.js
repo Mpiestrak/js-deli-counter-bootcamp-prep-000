6d338dd14020b35d15e12de40d62c3a3ac36d24f
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-31 00:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ return "Currently serving " + katzDeliLine.shift() + ".";
 }
 
 var line = []
-function currentLine(katzDeliLine) {
-  let i = 0;
-  while( i < katzDeliLine.length) {
-    line.push(` `+[i+1]+`. ` + katzDeliLine[i])
-      i--;
-  }
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  }
-  else return `The line is currently:` + line;
+function currentLine(katzDeli) {
+
+   for (let i = 0; i < katzDeli.length; i++) {
+     line.push(` `+[i+1]+`. `  + katzDeli[i])
+   }
+   return(`The line is currently:` + line);
 }
